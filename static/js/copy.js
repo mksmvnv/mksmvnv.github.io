947761a343ba72910d0b842b4ba677e5b65f1684
Maksim <mksmvnv@icloud.com>
2025-05-30 22:47:06 +0300

    feat: update animation

--- static/js/copy.js
+++ static/js/copy.js
@@ -1,7 +1,24 @@
 // Copy to clipboard
 
 const copyText = document.getElementById("copyText");
+const copyToast = new bootstrap.Toast(document.getElementById("copyToast"));
 
-copyText.addEventListener("click", () => {
-  navigator.clipboard.writeText(document.getElementById("copyText").textContent);
+copyText.addEventListener("click", async () => {
+  try {
+    await navigator.clipboard.writeText(copyText.textContent);
+
+    copyToast.show();
+
+    setTimeout(() => {
+      copyText.style.cursor = "pointer";
+      copyText.innerHTML = "@mksmvnv";
+    }, 1000);
+  } catch (err) {
+    console.error("Copy error:", err);
+    copyToast._element.querySelector(".toast-body").innerHTML = `
+      <i class="bi bi-x-circle-fill text-danger me-2"></i>
+      Copy error!
+    `;
+    copyToast.show();
+  }
 });
